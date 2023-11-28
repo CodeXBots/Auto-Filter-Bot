c441e9bf1538a061bc178ab36d4a3aec94e3a15b
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2023-11-28 19:16:44 +0530

    Add files via upload

---
+++ database/join_reqs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+import motor.motor_asyncio
+from info import REQ_CHANNEL
+
+class JoinReqs:
+
+    def __init__(self):
+        from info import JOIN_REQS_DB
+        if JOIN_REQS_DB:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(JOIN_REQS_DB)
+            self.db = self.client["JoinReqs"]
+            self.col = self.db[str(REQ_CHANNEL)]
+        else:
+            self.client = None
+            self.db = None
+            self.col = None
+
+    def isActive(self):
+        if self.client is not None:
+            return True
+        else:
+            return False
+
+    async def add_user(self, user_id, first_name, username, date):
+        try:
+            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
+        except:
+            pass
+
+    async def get_user(self, user_id):
+        return await self.col.find_one({"user_id": int(user_id)})
+
+    async def get_all_users(self):
+        return await self.col.find().to_list(None)
+
+    async def delete_user(self, user_id):
+        await self.col.delete_one({"user_id": int(user_id)})
+
+    async def delete_all_users(self):
+        await self.col.delete_many({})
+
+    async def get_all_users_count(self):
+        return await self.col.count_documents({})
