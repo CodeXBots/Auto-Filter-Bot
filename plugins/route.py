34638f81ea8610bc6b64844e0d3cea493445a5a9
Dinesh V <dinesh.v@zohocorp.com>
2024-01-26 14:05:10 +0530

    authu channel updated

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Nobita-Filter-Bot")
+    return web.json_response("Filter-pro-Bot")
