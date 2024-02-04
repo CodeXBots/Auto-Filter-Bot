c8e5f429127c95ea7cb23f9f4679a13f384f691e
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2024-02-04 17:48:38 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -10,7 +10,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
-        self.users = mydb.uersz
+        self.users = self.db.uersz
 
 
     def new_user(self, id, name):
