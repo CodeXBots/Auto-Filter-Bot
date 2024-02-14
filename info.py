4e45f671c058847f118829c73e4421ef39c027d5
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2024-02-14 22:48:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Nobita")
+DATABASE_URI = environ.get("DATABASE_URI", "")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Nobita")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # stickers
