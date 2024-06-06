a9ac126abde0b9894e6d6daf8a433ec2b9c63c0b
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-06-06 23:07:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NobiDeveloper/Nobita-Filter-Bot/tree/main.git /Nobita-Filter-Bot
+  git clone https://github.com/CodeXBots/Nobita-Filter-Bot/tree/main.git /Nobita-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Nobita-Filter-Bot
