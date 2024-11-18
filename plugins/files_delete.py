6f4e61ca5d4b9b3f153618a78131b116d57c1091
CodeXBots <RahulReviewsYT@gmail.com>
2024-11-18 14:25:01 +0530

    Create files_delete.py

---
+++ plugins/files_delete.py
@@ -0,0 +1,43 @@
+import re
+import logging
+from pyrogram import Client, filters
+from info import DELETE_CHANNELS
+from database.ia_filterdb import Media, unpack_new_file_id
+
+logger = logging.getLogger(__name__)
+
+media_filter = filters.document | filters.video | filters.audio
+
+
+@Client.on_message(filters.chat(DELETE_CHANNELS) & media_filter)
+async def deletemultiplemedia(bot, message):
+    """Delete Multiple files from database"""
+
+    for file_type in ("document", "video", "audio"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
+
+    file_id, file_ref = unpack_new_file_id(media.file_id)
+    if await Media.count_documents({'file_id': file_id}):
+        result = await Media.collection.delete_one({
+            '_id': file_id,
+        })
+    else:
+        logger.info(f'File with file_id {file_id} not found in Media collection.')
+
+    if result.deleted_count:
+        logger.info('File is successfully deleted from database.')
+    else:
+        file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+        result = await Media.collection.delete_many({
+            'file_name': file_name,
+            'file_size': media.file_size,
+            'mime_type': media.mime_type
+        })
+        if result.deleted_count:
+            logger.info('File is successfully deleted from database.')
+        else:
+            logger.info(f'File with name {file_name} not found in Media collection.')
