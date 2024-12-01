9779acd35a29012ef1791cffb4a8e7de06b62b40
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-01 21:53:04 +0530

    Update TechifyBots.py

--- plugins/helper/TechifyBots.py
+++ plugins/helper/TechifyBots.py
@@ -54,7 +54,7 @@ async def upload_command(client, message):
     await uploading_message.delete()
     codexbots=await message.reply_photo(
         photo=f'{url}',
-        caption=f"<b>ʏᴏᴜʀ ᴄʟᴏᴜᴅ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n𝑳𝒊𝒏𝒌 :-\n\n<code>{url}</code>\n\n<b>ᴘᴏᴡᴇʀᴇᴅ ʙʏ - @RahulReviewsYT</b>",
+        caption=f"<b>ʏᴏᴜʀ ᴄʟᴏᴜᴅ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n𝑳𝒊𝒏𝒌 :-\n\n<code>{url}</code>\n\n<b>ᴘᴏᴡᴇʀᴇᴅ ʙʏ - @TechifyBots</b>",
         #disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([[
             InlineKeyboardButton(text="• ᴏᴘᴇɴ ʟɪɴᴋ •", url=url),
